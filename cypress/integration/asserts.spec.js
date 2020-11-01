f0ead1aafb56cf2c626873de84f44abf3c3cf303
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-11-01 15:55:49 -0300

    Assertivas de veracidade

--- cypress/integration/asserts.spec.js
+++ cypress/integration/asserts.spec.js
@@ -9,3 +9,15 @@ it('Assertivas de Igualdade', () => {
    // expect(a).to.be.equal('b');
     expect(a).not.to.be.equal('b');
 })
+
+it('Verdadeiros ou não', () => {
+    const a = true;
+    const b = null;
+    let c;
+
+    expect(a).to.be.true
+    expect(b).to.be.null
+    expect(a).not.to.be.null
+    expect(a).to.be.not.null
+    expect(c).to.be.undefined
+})
