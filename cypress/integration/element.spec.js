36adcc0ab27c7fa8fbfe496bdb1c3aaf4cb2ef6d
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-11-02 15:45:07 -0300

    Hooks before e beforeEach

--- cypress/integration/element.spec.js
+++ cypress/integration/element.spec.js
@@ -1,26 +1,35 @@
 /// <reference types="cypress" />
 
 describe('Trabalhando com elementos', () => {
-    it('Textos', () => {
-        cy.visit('https://www.wcaquino.me/cypress/componentes.html');
 
+    //Executado antes de todos os testes
+    before(() => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+    })
+
+    //Executado antes de cada um dos testes
+    beforeEach(() => {
+        //cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+        cy.reload()
+    })
+
+    it('Textos', () => {
         cy.get('body').should('contain', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('contain', 'Cuidado')
-
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
 
     });
 
-    it.only('Links ou Botões', () => {
-        cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+    it('Links e Botões', () => {
         cy.get('[href="#"]').click();
-        cy.get('#resultado').should('have.text' , 'Voltou!')
+        cy.get('#resultado').should('have.text', 'Voltou!');
 
+        //Atualiza a pagina
         cy.reload()
-        cy.get('#resultado').should('have.not.text' , 'Voltou!')
-        cy.contains('Voltar').click();
-        cy.get('#resultado').should('have.text' , 'Voltou!');
 
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click();
+        cy.get('#resultado').should('have.text', 'Voltou!');
     })
 })
