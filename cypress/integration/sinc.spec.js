6e5c695216cfde037cf28763f377743cdaf5236e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-11-06 17:48:01 -0300

    Cuidado com o retrys na busca...

--- cypress/integration/sinc.spec.js
+++ cypress/integration/sinc.spec.js
@@ -26,15 +26,27 @@ describe('Entendendo a espera do Cypress', () => {
 
     });
 
-    it.only('Deve fazer retrys', () => {
+    it('Deve fazer retrys', () => {
 
         cy.get('#buttonDelay')
             .click()
 
         cy.get('#novoCampo')
-        //    .should('not.exist')
+            //    .should('not.exist')
             .should('exist')
             .type('funciona')
 
+    })
+
+    it.only('Usar busca', () => {
+
+        cy.get('#buttonList').click();
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1');
+
+        cy.get('#lista li')
+            //    .find('span')
+            .should('contain', 'Item 2');
     })
 })
