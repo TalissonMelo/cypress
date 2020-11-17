85e47676f843c4f7a45c035928af55582ee2e2db
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-11-17 18:30:52 -0300

    Its

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -32,4 +32,17 @@ describe('Helpers...', () => {
         //     return 2;
         // }).should('be.equal', 2);
     });
+
+    it.only('Its', () => {
+        const obj = { nome: 'Talisson', idade: 25 }
+        cy.wrap(obj).should('have.property', 'nome', 'Talisson');
+        cy.wrap(obj).its('nome').should('be.equal', 'Talisson');
+
+        const obj2 = { nome: 'Talisson', idade: 25 , endereco: {rua: 'Rua 02' , bairro : 'Ouro Verde'}}
+        cy.wrap(obj2).its('endereco').should('have.property', 'bairro', 'Ouro Verde');
+        cy.wrap(obj2).its('endereco').its('rua').should('contain', 'Rua 02' );
+
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+        cy.title().its('length').should('be.equal', 20);
+    });
 })
