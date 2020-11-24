ee98ddc1a8feff484d1621115b11d34fb73f7adf
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-11-24 19:00:44 -0300

    Confirm

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -12,16 +12,42 @@ describe('Trabalhando com alerta',() => {
     it('Alert', () => {
         cy.get('#alert').click();
         cy.on('window:alert', msg => {
-            console.log(msg);
             expect(msg).to.be.equal('Alert Simples');
         })
     });
 
-    it.only('Alert com mock', () => {
+    it('Alert com mock', () => {
         const stub = cy.stub().as('Alerta')
         cy.on('window:alert',stub)
         cy.get('#alert').click().then(() => {
             expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
         })
-    })
+    });
+
+    it.only('Confirm', () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples');
+        });
+        
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Confirmado');
+        })
+
+        cy.get('#confirm').click();
+    });
+
+    
+    it.only('Deny ', () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples');
+            return false;
+        });
+        
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Negado');
+        })
+
+        cy.get('#confirm').click();
+    });
+
 })
