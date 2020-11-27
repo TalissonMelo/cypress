02d65fc211c8ca92af84d01eab4fb86d903dbb84
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-11-27 20:29:02 -0300

    IFrame Pt2

--- cypress/integration/iframe.spec.js
+++ cypress/integration/iframe.spec.js
@@ -1,26 +1,25 @@
 /// <reference types="cypress" />
 
 describe('Trabalhando com IFrame', () => {
-    before(() => {
-        cy.visit('https://www.wcaquino.me/cypress/componentes.html');
-    })
-
-    beforeEach(() => {
-        cy.reload()
-    })
 
     it('Deve preencher campo de texto', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html');
         cy.get('#frame1').then(iframe => {
             const body = iframe.contents().find('body');
             cy.wrap(body).find('#tfield')
                 .type('funciona?')
                 .should('have.value', 'funciona?');
-            cy.on('window:alert', msg => {
-                expect(msg).to.be.equal('Alert Simples');
-            })
-            cy.wrap(body).find('#otherButton').click();
-        })
+        });
 
+    });
+
+    it('Deve testar frame diretamente', () => {
+        cy.visit('https://www.wcaquino.me/cypress/frame.html');
+        cy.get('#otherButton').click();
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Click OK!');
+        })
     })
+
 
 });
