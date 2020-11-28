54a38bd23c8794627d0b92cbae19451458c77ee4
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-11-27 22:29:39 -0300

    PopUp via links

--- cypress/integration/popup.spec.js
+++ cypress/integration/popup.spec.js
@@ -9,12 +9,40 @@ describe('Trabalhando com Popup', () => {
         });
     });
 
-    it.only('Deve vreificar se popup foi invocado', () => {
+    it.only('Deve verificar se popup foi invocado', () => {
         cy.visit('https://www.wcaquino.me/cypress/componentes.html');
         cy.window().then(win => {
             cy.stub(win, 'open').as('winOpen');
         });
         cy.get('#buttonPopUp').click();
         cy.get('@winOpen').should('be.called')
+    });
+
+});
+
+describe.only('PopUp com links', () => {
+    beforeEach(() => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+    });
+
+    it('Deve verificar se popup foi clicado', () => {
+        cy.contains('Popup2')
+            .should('have.prop', 'href')
+            .and('equal', 'https://www.wcaquino.me/cypress/frame.html')
+    });
+
+    it('Acessar PouUp', () => {
+        cy.contains('Popup2').then($a => {
+            const href = $a.prop('href');
+            cy.visit(href);
+            cy.get('#tfield').type('Funciona')
+        })
+    })
+
+    it('Link PopUp', () => {
+        cy.contains('Popup2')
+            .invoke('removeAttr', 'target')
+            .click()
+        cy.get('#tfield').type('Funciona')
     });
 });
