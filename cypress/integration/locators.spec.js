d84cfb50418c644f33a2c00b75f2828525d9e123
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-11-28 06:52:47 -0300

    Locators

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -10,7 +10,10 @@ describe('Trabalhando com elementos', () => {
         cy.reload()
     });
 
-    it('...', () => {
-        
+    //Seletores jQuery https://www.w3schools.com/jquery/jquery_ref_selectors.asp
+    it('Usando jQuery Selectors', () => {
+        cy.get('table#tabelaUsuarios tbody > tr:eq(0) td:nth-child(3) > input').click();
+        cy.get('[onclick*=\'Francisco\']')
+        cy.get("[onclick*='Francisco']")
     })
 });
