cfa1eb86a78848435064c83caa83b3afc9e9defe
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-11-30 18:42:08 -0300

    Inserindo Conta

--- cypress/integration/testesApi.spec.js/backend.spec.js
+++ cypress/integration/testesApi.spec.js/backend.spec.js
@@ -23,7 +23,32 @@ describe('Testes de API', () => {
         }).its('body.token').should('not.be.empty');
     })
 
-    it('Deve inserir uma conta', () => {
+    it.only('Deve inserir uma conta', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://barrigarest.wcaquino.me/signin',
+            body: {
+                email: "TA@",
+                redirecionar: false,
+                senha: "123"
+            }
+        }).its('body.token').should('not.be.empty')
+            .then(token => {
+            cy.request({
+                method: 'POST',
+                headers: { Authorization: `JWT ${token}` },
+                url: 'http://barrigarest.wcaquino.me/contas',
+                body: {
+                    nome: "Conta testes API"
+                }
+            }).as('response')
+        })
+
+        cy.get('@response').then(res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('id')
+            expect(res.body).to.have.property('nome', 'Conta testes API')
+        })
     });
 
     it('Deve alterar uma conta', () => {
