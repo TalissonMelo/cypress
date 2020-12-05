b9527004b91e6c66d449a9a0d6081bdcb6006811
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-12-05 14:16:02 -0300

    Inserir Conta

--- cypress/integration/testesInterface/frontend.spec.js
+++ cypress/integration/testesInterface/frontend.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 
+import loc from '../../support/locators'
 import '../../support/commandsContas'
 
 describe('Testes Funcionais', () => {
@@ -26,20 +27,75 @@ describe('Testes Funcionais', () => {
             url: '/saldo',
             response: [{
                 conta_id: 8999,
-                conta : "Conta falsa movimentação",
+                conta: "Conta falsa movimentação",
                 saldo: 1000
             },
             {
                 conta_id: 90000,
-                conta : "Conta falsa",
+                conta: "Conta falsa",
                 saldo: 1
             }]
         }).as('saldo')
         cy.login('USU@', '123');
     });
 
-    it('Deve inserir uma conta', () => {
+    it.only('Deve inserir uma conta', () => {
 
+        cy.route({
+            method: 'GET',
+            url: '/contas',
+            response: [{
+                id: 1,
+                nome: "Conta falsa movimentação",
+                visivel: true,
+                usuario_id: 1
+            },
+            {
+                id: 2,
+                nome: "Conta falsa",
+                visivel: true,
+                usuario_id: 1
+            }]
+        }).as('contas');
+
+        cy.route({
+            method: 'POST',
+            url: '/contas',
+            response: {
+                id: 3,
+                nome: "Conta Teste Cypress",
+                visivel: true,
+                usuario_id: 1
+            }
+        }).as('SalvarConta')
+
+        cy.acessarMenuConta();
+
+        cy.route({
+            method: 'GET',
+            url: '/contas',
+            response: [{
+                id: 1,
+                nome: "Conta falsa movimentação",
+                visivel: true,
+                usuario_id: 1
+            },
+            {
+                id: 2,
+                nome: "Conta falsa",
+                visivel: true,
+                usuario_id: 1
+            },
+            {
+                id: 3,
+                nome: "Conta Teste Cypress",
+                visivel: true,
+                usuario_id: 1
+            }]
+        }).as('ListarContasSalvas');
+
+        cy.inserirConta('Conta Teste Cypress');
+        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     });
 
     it('Deve alterar uma conta', () => {
