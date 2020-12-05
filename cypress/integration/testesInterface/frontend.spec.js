c4e2697caae1c2a8ce0e1a75cba605582b209dab
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-12-05 14:32:45 -0300

    Alterando Conta

--- cypress/integration/testesInterface/frontend.spec.js
+++ cypress/integration/testesInterface/frontend.spec.js
@@ -56,7 +56,29 @@ describe('Testes Funcionais', () => {
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     });
 
-    it('Deve alterar uma conta', () => {
+    it.only('Deve alterar uma conta', () => {
+        
+        cy.route({
+            method: 'GET',
+            url: '/contas',
+            response: [{id: 1,nome: "Conta falsa movimentação", visivel: true, usuario_id: 1 },
+            {id: 2, nome: "Conta falsa", visivel: true, usuario_id: 1 },
+            { id: 3, nome: "Conta Teste Cypress",  visivel: true,  usuario_id: 1}]
+        }).as('ListarContasSalvas');
+
+        cy.route({
+            method: 'PUT',
+            url: '/contas/**',
+            response: { id: 3, nome: "Conta Alterada com sucesso", visivel: true, usuario_id: 1}
+        }).as('SalvarConta')
+
+        cy.acessarMenuConta();
+        cy.xpath(loc.CONTAS.XP_BTN_EDT).click();
+        cy.get(loc.CONTAS.NOME)
+            .clear()
+            .type('Conta Alterada com sucesso');
+        cy.get(loc.CONTAS.BTN_SALVAR).click();
+        cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso!')
     });
 
     it('Não deve inserir conta com mesmo nome!.', () => {
