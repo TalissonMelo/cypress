c6a8eed59c1b68666f0a747050dac0743492550e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-12-05 14:20:46 -0300

    Refactor preview

--- cypress/integration/testesInterface/frontend.spec.js
+++ cypress/integration/testesInterface/frontend.spec.js
@@ -15,26 +15,14 @@ describe('Testes Funcionais', () => {
         cy.route({
             method: 'POST',
             url: '/signin',
-            response: {
-                id: 1000,
-                nome: 'Usuário Falso',
-                token: 'Uma string muito grande que não pode ser aceita, entretanto vai ser aceita'
-            }
+            response: {id: 1000, nome: 'Usuário Falso', token: 'Uma string muito grande que não pode ser aceita, entretanto vai ser aceita' }
         }).as('signin')
 
         cy.route({
             method: 'GET',
             url: '/saldo',
-            response: [{
-                conta_id: 8999,
-                conta: "Conta falsa movimentação",
-                saldo: 1000
-            },
-            {
-                conta_id: 90000,
-                conta: "Conta falsa",
-                saldo: 1
-            }]
+            response: [{ conta_id: 8999, conta: "Conta falsa movimentação", saldo: 1000 },
+            { conta_id: 90000, conta: "Conta falsa", saldo: 1}]
         }).as('saldo')
         cy.login('USU@', '123');
     });
@@ -44,29 +32,14 @@ describe('Testes Funcionais', () => {
         cy.route({
             method: 'GET',
             url: '/contas',
-            response: [{
-                id: 1,
-                nome: "Conta falsa movimentação",
-                visivel: true,
-                usuario_id: 1
-            },
-            {
-                id: 2,
-                nome: "Conta falsa",
-                visivel: true,
-                usuario_id: 1
-            }]
+            response: [{ id: 1, nome: "Conta falsa movimentação", visivel: true, usuario_id: 1 },
+            { id: 2, nome: "Conta falsa", visivel: true, usuario_id: 1 }]
         }).as('contas');
 
         cy.route({
             method: 'POST',
             url: '/contas',
-            response: {
-                id: 3,
-                nome: "Conta Teste Cypress",
-                visivel: true,
-                usuario_id: 1
-            }
+            response: { id: 3, nome: "Conta Teste Cypress", visivel: true, usuario_id: 1}
         }).as('SalvarConta')
 
         cy.acessarMenuConta();
@@ -74,24 +47,9 @@ describe('Testes Funcionais', () => {
         cy.route({
             method: 'GET',
             url: '/contas',
-            response: [{
-                id: 1,
-                nome: "Conta falsa movimentação",
-                visivel: true,
-                usuario_id: 1
-            },
-            {
-                id: 2,
-                nome: "Conta falsa",
-                visivel: true,
-                usuario_id: 1
-            },
-            {
-                id: 3,
-                nome: "Conta Teste Cypress",
-                visivel: true,
-                usuario_id: 1
-            }]
+            response: [{id: 1,nome: "Conta falsa movimentação", visivel: true, usuario_id: 1 },
+            {id: 2, nome: "Conta falsa", visivel: true, usuario_id: 1 },
+            { id: 3, nome: "Conta Teste Cypress",  visivel: true,  usuario_id: 1}]
         }).as('ListarContasSalvas');
 
         cy.inserirConta('Conta Teste Cypress');
