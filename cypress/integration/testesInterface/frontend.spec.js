9a8830f458b9202c7d472fb05cfbe6fa2781dda6
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-12-06 07:56:26 -0300

    *** Gerenciamento em massa Routes

--- cypress/integration/testesInterface/frontend.spec.js
+++ cypress/integration/testesInterface/frontend.spec.js
@@ -2,44 +2,25 @@
 
 import loc from '../../support/locators'
 import '../../support/commandsContas'
+import buildRoute from '../../support/buildRoute'
 
-describe('Testes Funcionais', () => {
+describe('Testes Interface', () => {
 
     after(() => {
         cy.clearLocalStorage();
     })
 
 
-    before(() => {
-        cy.server()
-        cy.route({
-            method: 'POST',
-            url: '/signin',
-            response: {id: 1000, nome: 'Usuário Falso', token: 'Uma string muito grande que não pode ser aceita, entretanto vai ser aceita' }
-        }).as('signin')
-
-        cy.route({
-            method: 'GET',
-            url: '/saldo',
-            response: [{ conta_id: 8999, conta: "Conta falsa movimentação", saldo: 1000 },
-            { conta_id: 90000, conta: "Conta falsa", saldo: 1}]
-        }).as('saldo')
+    beforeEach(() => {
+        buildRoute()
         cy.login('USU@', '123');
     });
 
-    it.only('Deve inserir uma conta', () => {
-
-        cy.route({
-            method: 'GET',
-            url: '/contas',
-            response: [{ id: 1, nome: "Conta falsa movimentação", visivel: true, usuario_id: 1 },
-            { id: 2, nome: "Conta falsa", visivel: true, usuario_id: 1 }]
-        }).as('contas');
-
+    it('Deve inserir uma conta', () => {
         cy.route({
             method: 'POST',
             url: '/contas',
-            response: { id: 3, nome: "Conta Teste Cypress", visivel: true, usuario_id: 1}
+            response: { id: 3, nome: "Conta Teste Cypress", visivel: true, usuario_id: 1 }
         }).as('SalvarConta')
 
         cy.acessarMenuConta();
@@ -47,29 +28,29 @@ describe('Testes Funcionais', () => {
         cy.route({
             method: 'GET',
             url: '/contas',
-            response: [{id: 1,nome: "Conta falsa movimentação", visivel: true, usuario_id: 1 },
-            {id: 2, nome: "Conta falsa", visivel: true, usuario_id: 1 },
-            { id: 3, nome: "Conta Teste Cypress",  visivel: true,  usuario_id: 1}]
+            response: [{ id: 1, nome: "Conta falsa movimentação", visivel: true, usuario_id: 1 },
+            { id: 2, nome: "Conta falsa", visivel: true, usuario_id: 1 },
+            { id: 3, nome: "Conta Teste Cypress", visivel: true, usuario_id: 1 }]
         }).as('ListarContasSalvas');
 
         cy.inserirConta('Conta Teste Cypress');
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     });
 
-    it.only('Deve alterar uma conta', () => {
-        
+    it('Deve alterar uma conta', () => {
+
         cy.route({
             method: 'GET',
             url: '/contas',
-            response: [{id: 1,nome: "Conta falsa movimentação", visivel: true, usuario_id: 1 },
-            {id: 2, nome: "Conta falsa", visivel: true, usuario_id: 1 },
-            { id: 3, nome: "Conta Teste Cypress",  visivel: true,  usuario_id: 1}]
+            response: [{ id: 1, nome: "Conta falsa movimentação", visivel: true, usuario_id: 1 },
+            { id: 2, nome: "Conta falsa", visivel: true, usuario_id: 1 },
+            { id: 3, nome: "Conta Teste Cypress", visivel: true, usuario_id: 1 }]
         }).as('ListarContasSalvas');
 
         cy.route({
             method: 'PUT',
             url: '/contas/**',
-            response: { id: 3, nome: "Conta Alterada com sucesso", visivel: true, usuario_id: 1}
+            response: { id: 3, nome: "Conta Alterada com sucesso", visivel: true, usuario_id: 1 }
         }).as('SalvarConta')
 
         cy.acessarMenuConta();
---
+++ cypress/support/buildRoute.js
@@ -0,0 +1,28 @@
+const buildRoute = () => {
+
+    cy.server()
+    cy.route({
+        method: 'POST',
+        url: '/signin',
+        response: { id: 1000, nome: 'Usuário Falso', token: 'Uma string muito grande que não pode ser aceita, entretanto vai ser aceita' }
+    }).as('signin')
+
+    cy.route({
+        method: 'GET',
+        url: '/saldo',
+        response: [{ conta_id: 8999, conta: "Conta falsa movimentação", saldo: 1000 },
+        { conta_id: 90000, conta: "Conta falsa", saldo: 1 }]
+    }).as('saldo')
+
+
+    cy.route({
+        method: 'GET',
+        url: '/contas',
+        response: [{ id: 1, nome: "Conta falsa movimentação", visivel: true, usuario_id: 1 },
+        { id: 2, nome: "Conta falsa", visivel: true, usuario_id: 1 }]
+    }).as('contas');
+
+
+}
+
+export default buildRoute;
